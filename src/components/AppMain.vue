ef2f12e02117babd76930128c3e470c61bdc922a
Shamble94 <fratus.nicholas@gmail.com>
2024-03-01 20:18:11 +0100

    Gestita paginazione con bottoni che si disabilitano se ci si trova sulla prima o ultima pagina. Esercizio e bonus completati

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,16 +15,25 @@ export default {
         return {
             store,
             projects: [],
-            
+            currentPage: 1,
+            lastPage: null
+
         }
     },
     created(){
         this.getProjects();
     },
     methods:{
-        getProjects(){
-            axios.get(`${this.store.baseUrl}/api/projects`).then((response) => {
+        getProjects(page_number){
+            axios.get(`${this.store.baseUrl}/api/projects`,
+            {
+                params:{
+                    page : page_number
+                }
+            }).then((response) => {
                this.projects = response.data.results.data;
+               this.currentPage = response.data.results.current_page;
+               this.lastPage = response.data.results.last_page;
 
             })
         }
@@ -39,13 +48,18 @@ export default {
 
                 <h1>Boolfolio</h1>
                 <h4>Raccolta dei progetti </h4>
+                <button class="btn btn-sm btn-primary mx-3" :class="currentPage == 1 ? `disabled` : ``" @click="getProjects(currentPage - 1)">Indietro</button>
+                <button class="btn btn-sm btn-primary"  :class="currentPage == lastPage ? `disabled` : ``" @click="getProjects(currentPage + 1)">Avanti</button>
             </div>
         </div>
+       
+            
+        
+      
         <div class="row">
             <ProjectCard v-for="project, index  in projects" :key="index" :project="project"/>
         </div>
     </div>
-  
 </template>
     
 <style  lang="scss" scoped>
